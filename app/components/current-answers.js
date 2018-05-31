e51062c7a28b65bdc27fb8a40cf2d22c217176e7
Max Antonucci <maxx1128@gmail.com>
2018-05-31 17:49:06 -0400

    Added point scaling for answers

--- app/components/current-answers.js
+++ app/components/current-answers.js
@@ -9,17 +9,30 @@ export default Component.extend({
   answers: computed('questions_data', 'question_number', function(){
     let question = this.get('questions_data').objectAt(this.get('question_number'));
 
-    console.log(question);
-
     return question.answers;
   }),
 
   actions: {
     select_answer(user, question_number, correct) {
-      let points = correct ? 5 : 0;
+      let final_points,
+          base_points = 5,
+          timing_limit = 10,
+          current_answers = this.get('number_of_answers'),
+          timing_rank = ((timing_limit - current_answers) / 2);
+
+      if ((current_answers < timing_limit) && (timing_rank >= 1)) {
+        base_points = correct ? 5 : -2.5;
+        
+        final_points = (base_points * timing_rank);
+      } else {
+        final_points = correct ? base_points : 0;
+      }
+
+      console.log(timing_rank);
+      console.log(final_points);
 
       this.set('answered_state', true);
-      this.get('quiz').submit_answer(user, question_number, correct, points);
+      this.get('quiz').submit_answer(user, question_number, correct, final_points);
     }
   },
 

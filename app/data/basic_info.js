c0bf3861df548ad8a91bdd0a677e7862ccce0cc4
Max Antonucci <maxx1128@gmail.com>
2018-06-06 12:31:58 -0400

    Added basic image component

--- app/data/basic_info.js
+++ app/data/basic_info.js
@@ -1,7 +1,7 @@
 const basic_info = {
   title: "Quiz Title",
   subheading: "A basic description of your quiz game here!",
-  qr_code: "https://cdn-images-1.medium.com/max/1440/1*kYT6nGA-NeGit2TDTfm-bw.png"
+  home_image: "magic_tavern.png"
 }
 
 export default basic_info;

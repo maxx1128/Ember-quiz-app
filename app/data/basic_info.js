5a183e9b0d71794363ab57bef5de7d66d855420d
Max Antonucci <maxx1128@gmail.com>
2018-06-07 12:42:05 -0400

    Converted image to external url

--- app/data/basic_info.js
+++ app/data/basic_info.js
@@ -1,7 +1,7 @@
 const basic_info = {
   title: "Quiz Title",
   subheading: "A basic description of your quiz game here!",
-  home_image: "magic_tavern.png"
+  home_image: "https://cdn-images-1.medium.com/max/1440/1*kYT6nGA-NeGit2TDTfm-bw.png"
 }
 
 export default basic_info;

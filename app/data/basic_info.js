cf86692de36ab654cd9674ddbf1908e098edcfe3
Max Antonucci <maxx1128@gmail.com>
2018-09-19 13:05:13 -0400

    Added quiz link to home screen

--- app/data/basic_info.js
+++ app/data/basic_info.js
@@ -1,6 +1,6 @@
 const basic_info = {
   title: "NewHaven.IO JavaScript Trivia",
-  subheading: "Test your basic, and obscure, JavaScript knowledge",
+  subheading: "Test your basic, and obscure, JavaScript knowledge with us! Join the quiz at https://newhavenio-jstrivia.netlify.com/",
   home_image: "https://cdn-images-1.medium.com/max/1200/1*H-25KB7EbSHjv70HXrdl6w.png"
 }
 

ad47fd25bbc4dc0022fe79030cf34d5572596648
Max Antonucci <maxx1128@gmail.com>
2018-09-19 12:46:51 -0400

    Add questions

--- app/data/questions.js
+++ app/data/questions.js
@@ -1,43 +1,217 @@
 const questions = [
   {
-    'q_text': 'What is 2 plus 2?',
-    'q_intro': 'Nulla sunt labore irure do culpa laborum irure aliquip incididunt Lorem minim anim. Sit amet deserunt reprehenderit nostrud sunt voluptate est do ea ex laborum irure mollit enim. Ullamco consectetur voluptate consectetur veniam sint Lorem ullamco. Cillum ad et nostrud occaecat veniam voluptate ea sit eu officia ad tempor officia. Reprehenderit labore Lorem commodo in minim laborum. Sint laboris proident eu sit reprehenderit Lorem voluptate est. Cupidatat mollit minim consequat laborum nostrud eiusmod exercitation eiusmod minim.',
-    'q_image': 'https://img00.deviantart.net/a976/i/2015/305/e/0/carnival_wilson_by_erinptah-d9f611r.png',
-    'a_text': 'Magna sunt ipsum adipisicing sunt enim. Pariatur amet in nisi consectetur mollit qui aliqua non. Ea aute ad magna ea reprehenderit cupidatat in Lorem mollit est. Proident ad et reprehenderit ea officia et officia adipisicing incididunt mollit cupidatat ad eiusmod in. Eiusmod tempor occaecat eu dolore id quis proident id. Sunt mollit non esse elit ea laborum sit velit officia sint proident est labore.',
-    'a_image': 'https://i1.sndcdn.com/artworks-000191221487-8m77vu-t500x500.jpg',
+    'q_text': 'Which is NOT a way to declare a variable?',
+    'a_text': '"new" is used for declaring new class instances! The others are the typical ways for declaring new variables.',
     'answers':[
       {
-        'text': '4',
+        'text': 'var',
+        'correct': false
+      }, {
+        'text': 'let',
+        'correct': false
+      }, {
+        'text': 'new',
         'correct': true
       }, {
-        'text': '5',
-        'correct': false
-      }, {
-        'text': '7',
-        'correct': false
-      }, {
-        'text': '15',
+        'text': 'const',
         'correct': false
       }
     ]
-  }, {
-    'q_text': 'What is 3 plus 3?',
-    'q_intro': 'Ea voluptate ex incididunt voluptate consectetur anim. Elit mollit labore nulla exercitation et non. Lorem excepteur et ea sunt sunt pariatur sunt officia enim labore aute deserunt voluptate qui. Minim ullamco et magna laborum exercitation commodo deserunt proident esse. Est exercitation exercitation sint duis commodo.',
-    'q_image': 'https://img00.deviantart.net/a976/i/2015/305/e/0/carnival_wilson_by_erinptah-d9f611r.png',
-    'a_text': 'Ex voluptate dolor qui do laborum commodo ipsum ullamco in sint eiusmod non cupidatat. Sit consequat laborum sunt eiusmod proident qui. Pariatur commodo laboris amet fugiat eiusmod do est ex enim.',
-    'a_image': 'https://i1.sndcdn.com/artworks-000191221487-8m77vu-t500x500.jpg',
+  },
+  {
+    'q_text': 'What will the following code return?',
+    'q_image': 'https://image.ibb.co/hXWy9z/Screen_Shot_2018_09_18_at_8_00_18_PM.png',
+    'a_text': 'The `.some` function returns true if at least one item in the array meets the condition. Since at least one number in this array is greater than 18, it returns true.',
+    'a_image': 'https://image.ibb.co/hXWy9z/Screen_Shot_2018_09_18_at_8_00_18_PM.png',
     'answers':[
       {
-        'text': '6',
+        'text': '[22, 31, 25]',
+        'correct': false
+      }, {
+        'text': '[9, 17]',
+        'correct': false
+      }, {
+        'text': 'true',
         'correct': true
       }, {
-        'text': '5',
+        'text': '[true, false, false, true, true]',
         'correct': false
       }, {
-        'text': '7',
+        'text': '["voter", "baby", "baby", "voter", "voter"]',
+        'correct': false
+      }
+    ]
+  },
+  {
+    'q_text': 'Which of these is NOT a JavaScript framework?',
+    'a_text': 'React is a UI library and is not technically a framework. The other options are full frameworks of various ages and strengths.',
+    'answers':[
+      {
+        'text': 'React',
+        'correct': true
+      }, {
+        'text': 'Vue',
         'correct': false
       }, {
-        'text': '15',
+        'text': 'Ember',
+        'correct': false
+      }, {
+        'text': 'Backbone',
+        'correct': false
+      }, {
+        'text': 'Mithril',
+        'correct': false
+      }
+    ]
+  },
+  {
+    'q_text': 'What is the most popular tool today for writing server-side JavaScript?',
+    'a_text': 'There may be others out there, but the most obvious one today (and with the most jobs for it) is Node.',
+    'answers':[
+      {
+        'text': 'TypeScript',
+        'correct': false
+      }, {
+        'text': 'Netscape Enterprise',
+        'correct': false
+      }, {
+        'text': 'Redux',
+        'correct': false
+      }, {
+        'text': 'Node',
+        'correct': true
+      }, {
+        'text': 'Prayer',
+        'correct': false
+      }
+    ]
+  },
+  {
+    'q_text': 'What will this code return?',
+    'q_image': 'https://image.ibb.co/hn472K/Screen_Shot_2018_09_18_at_8_03_22_PM.png',
+    'a_text': 'Combining a boolean and string in this way will turn the boolean into a string and then stick them both together',
+    'a_image': 'https://image.ibb.co/hn472K/Screen_Shot_2018_09_18_at_8_03_22_PM.png',
+    'answers':[
+      {
+        'text': '"truexyz"',
+        'correct': true
+      }, {
+        'text': 'true',
+        'correct': false
+      }, {
+        'text': '"xyz"',
+        'correct': false
+      }, {
+        'text': '"1xyz"',
+        'correct': false
+      }, {
+        'text': 'Yep, that\'s a string',
+        'correct': false
+      }
+    ]
+  },
+  {
+    'q_text': 'Which of the following is FALSE about methods and functions?',
+    'a_text': 'Methods can only be called as object properties, which functions can be called completely separate from objects.',
+    'answers':[
+      {
+        'text': 'They are called the same way',
+        'correct': true
+      }, {
+        'text': 'They are both types of functions',
+        'correct': false
+      }, {
+        'text': 'They cannot be associated with objects',
+        'correct': false
+      }, {
+        'text': 'They can be given parameters',
+        'correct': false
+      }, {
+        'text': 'They are both part of JavaScript',
+        'correct': false
+      }
+    ]
+  },
+  {
+    'q_text': 'What is the term for declaring new variables by extracting their values from objects or arrays?',
+    'a_image': 'https://image.ibb.co/iM36we/Screen_Shot_2018_09_20_at_11_52_30_AM.png',
+    'answers':[
+      {
+        'text': 'Destructuring',
+        'correct': true
+      }, {
+        'text': 'Abstraction',
+        'correct': false
+      }, {
+        'text': 'Assembly',
+        'correct': false
+      }, {
+        'text': 'Composition',
+        'correct': false
+      }, {
+        'text': 'Yankity-yanking',
+        'correct': false
+      }
+    ]
+  },
+  {
+    'q_text': 'What lets a function easily take any number of variables and combine them into an array?',
+    'a_image': 'https://image.ibb.co/nJF3be/Screen_Shot_2018_09_20_at_11_53_45_AM.png',
+    'answers':[
+      {
+        'text': 'A Spread Operator',
+        'correct': true
+      }, {
+        'text': 'An Array Factory',
+        'correct': false
+      }, {
+        'text': 'Concatenation',
+        'correct': false
+      }, {
+        'text': 'Splicing and Merging',
+        'correct': false
+      }, {
+        'text': 'Little Squishing',
+        'correct': false
+      }
+    ]
+  },
+  {
+    'q_text': 'What is the source of JavaScript\'s asynchronous functionality?',
+    'a_text': 'JavaScript itself is synchronous and only goes step-by-step. It borrows ansynchrounous functionality from the Web Browser API.',
+    'answers':[
+      {
+        'text': 'The Web Browser API',
+        'correct': true
+      }, {
+        'text': 'Callbacks',
+        'correct': false
+      }, {
+        'text': 'Async/Await',
+        'correct': false
+      }, {
+        'text': 'Wizards',
+        'correct': false
+      }
+    ]
+  },
+  {
+    'q_text': 'When JavaScript was in development, it was done so under a different name. What was this language\'s first name?',
+    'answers':[
+      {
+        'text': 'Mocha',
+        'correct': true
+      }, {
+        'text': 'LiveScript',
+        'correct': false
+      }, {
+        'text': 'Java',
+        'correct': false
+      }, {
+        'text': 'BrowserScript',
+        'correct': false
+      }, {
+        'text': 'RegretScript',
         'correct': false
       }
     ]
